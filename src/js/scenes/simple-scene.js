2dd1fc517e09c0f43616487e289a196fc7f26160
NewIncome <alfredo_cardenas@live.com>
2020-09-21 14:49:42 -0500

    Add basic config and setup for Phaser working with npm & webpack
    - add test code to run a basic Phaser scene
    - add the scenes dir
    - add a scene file with 'hello world' message

---
+++ src/js/scenes/simple-scene.js
@@ -0,0 +1,7 @@
+import Phaser from 'phaser';
+
+export default class SimpleScene extends Phaser.Scene {
+  create() {
+    this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+/* eslint-disable no-new */
+import './styles/style.scss';
+
+import Phaser from 'phaser';
+
+import simpleScene from './js/scenes/simple-scene';
+
+const gameConfig = {
+  width: 680,
+  height: 400,
+  scene: simpleScene,
+};
+
+new Phaser.Game(gameConfig);
