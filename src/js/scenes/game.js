3bc854ec876402fb518bcb097202c98f032e1e0e
NewIncome <alfredo_cardenas@live.com>
2020-09-21 22:15:56 -0500

    Add background on game scene Comment out openning scene for quicker testing

--- src/js/scenes/game.js
+++ src/js/scenes/game.js
@@ -1,1 +1,15 @@
-console.log('in game');
+import Phaser from 'phaser';
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super('game');
+  }
+
+  preload() {
+    this.load.image('game-bg', '../assets/bg&objects/Background.png');
+  }
+
+  create() {
+    this.add.image(400, 300, 'game-bg').setScale(0.45);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,15 +3,17 @@ import './styles/style.scss';
 
 import Phaser from 'phaser';
 
-import Openning from './js/scenes/openning';
+// import Openning from './js/scenes/openning';
 
 import Game from './js/scenes/game';
 
 export default new Phaser.Game({
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scene: [Openning, Game],
+  width: 1130,
+  height: 800,
+  scene: [
+    // Openning,
+    Game],
   physics: {
     default: 'arcade',
     arcade: {
