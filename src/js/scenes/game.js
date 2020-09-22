6a783804661b4c39c9cea6241aec0569efadfeb8
NewIncome <alfredo_cardenas@live.com>
2020-09-22 16:37:53 -0500

    Finish Layout with land and objects

--- src/js/scenes/game.js
+++ src/js/scenes/game.js
@@ -15,52 +15,30 @@ export default class Game extends Phaser.Scene {
   preload() {
     const asset = this.load.image('game-bg', '../assets/bg&objects/Background.png');
     // preLoadBar(this, asset, 'game-bg');
+
+    // ---------- Loading the Land ----------
     this.load.image('land-s', '../assets/bg&objects/land-sm.png');
     this.load.image('land-lg', '../assets/bg&objects/land-lg.png');
     this.load.image('land-flat', '../assets/bg&objects/land-ss.png');
 
-    tileLoader(this,
-      'tile-flat-',
-      ['l', 'm', 'r'],
-      '../assets/bg&objects/Tile_',
-      10,
-      3,
-      '.png');
+    tileLoader(this, 'tile-flat-', ['l', 'm', 'r'],
+      '../assets/bg&objects/Tile_', 10, 3, '.png');
 
-    // this.load.image('tile-flat-l', '../assets/bg&objects/Tile_10.png');
-    // this.load.image('tile-flat-m', '../assets/bg&objects/Tile_11.png');
-    // this.load.image('tile-flat-r', '../assets/bg&objects/Tile_12.png');
+    tileLoader(this, 'tile-md-', ['t', 'm', 'b'],
+      '../assets/bg&objects/Tile_', 13, 3, '.png');
 
-    tileLoader(this,
-      'tile-md-',
-      ['t', 'm', 'b'],
-      '../assets/bg&objects/Tile_',
-      13,
-      3,
-      '.png');
-
-    // this.load.image('tile-md-t', '../assets/bg&objects/Tile_13.png');
-    // this.load.image('tile-md-m', '../assets/bg&objects/Tile_14.png');
-    // this.load.image('tile-md-b', '../assets/bg&objects/Tile_15.png');
-
-    tileLoader(this,
-      'tile-lg-',
+    tileLoader(this, 'tile-lg-',
       ['tl', 'tm', 'tr', 'ml', 'mm', 'mr', 'bl', 'bm', 'br'],
-      '../assets/bg&objects/Tile_',
-      1,
-      9,
-      '.png');
+      '../assets/bg&objects/Tile_', 1, 9, '.png');
 
-    // this.load.image('tile-lg-tl', '../assets/bg&objects/Tile_1.png');
-    // this.load.image('tile-lg-tm', '../assets/bg&objects/Tile_2.png');
-    // this.load.image('tile-lg-tr', '../assets/bg&objects/Tile_3.png');
-    // this.load.image('tile-lg-ml', '../assets/bg&objects/Tile_4.png');
-    // this.load.image('tile-lg-mm', '../assets/bg&objects/Tile_5.png');
-    // this.load.image('tile-lg-mr', '../assets/bg&objects/Tile_6.png');
-    // this.load.image('tile-lg-bl', '../assets/bg&objects/Tile_7.png');
-    // this.load.image('tile-lg-bm', '../assets/bg&objects/Tile_8.png');
-    // this.load.image('tile-lg-br', '../assets/bg&objects/Tile_9.png');
+    // ---------- Loading Objects ----------
+    this.load.image('light-tree', '../assets/bg&objects/Objects/Object_16.png');
+    this.load.image('lessLgt-tree', '../assets/bg&objects/Objects/Object_18.png');
+    this.load.image('flower', '../assets/bg&objects/Objects/Object_1.png');
+    this.load.image('lgRock', '../assets/bg&objects/Objects/Object_2.png');
+    this.load.image('smRock', '../assets/bg&objects/Objects/Object_3.png');
 
+    // ---------- Loading Animated Objects ----------
     this.load.spritesheet('rabbit',
       '../assets/player&enemies/rabbit-sprite.png',
       { frameWidth: 54, frameHeight: 90 });
@@ -69,21 +47,32 @@ export default class Game extends Phaser.Scene {
   create() {
     this.add.image(400, 300, 'game-bg').setScale(0.4);
 
-    this.add.image(100, 200, 'land-s');
-    this.add.image(100, 400, 'land-lg');
-    this.add.image(100, 600, 'land-flat');
+    // Creating background objects
+    this.add.image(750, 150, 'lessLgt-tree').setScale(0.5);
+    this.add.image(50, 20, 'lgRock').setScale(0.7);
 
-    GndCreate.makeFlatLand(400, 500, this, 2);
-    GndCreate.makeFlatLand(400, 550, this, 3);
-    GndCreate.makeFlatLand(400, 600, this, 4);
+    // Creating the Land
+    GndCreate.makeLgLand2(-100, 100, this);
+    this.add.image(150, 300, 'light-tree').setScale(0.9);
+    GndCreate.makeLgLand1(350, 30, this);
+    GndCreate.makeLgLand2(550, 600, this);
+    GndCreate.makeLgLand3(0, 500, this); // 450~
+    GndCreate.makeMdLand(900, 170, this, 2); // Md
+    GndCreate.makeLgLand2(650, 270, this);
 
-    GndCreate.makeMdLand(200, 100, this, 2);
-    GndCreate.makeMdLand(250, 100, this, 3);
-    GndCreate.makeMdLand(300, 100, this, 4);
+    // this.add.image(100, 200, 'land-s');
+    // this.add.image(100, 400, 'land-lg');
+    this.add.image(570, 410, 'land-flat');
 
-    GndCreate.makeLgLand1(400, 50, this);
-    GndCreate.makeLgLand2(450, 100, this);
-    GndCreate.makeLgLand3(600, 150, this);
+    this.add.image(900, 417, 'smRock').setScale(0.8);
+
+    GndCreate.makeFlatLand(640, 120, this, 2);
+    GndCreate.makeFlatLand(660, 490, this, 5);
+    GndCreate.makeFlatLand(820, 400, this, 4);
+
+    GndCreate.makeMdLand(270, 120, this, 2);
+
+    this.add.image(700, 530, 'flower').setScale(0.5);
 
     const player = this.add.sprite(100, 600, 'rabbit');
   }
