c387d8b613d71b1953368b6f3e482a0ab6d3f929
NewIncome <alfredo_cardenas@live.com>
2020-09-21 20:53:51 -0500

    Fix problem with loading images
    - (it was just the url of the image)
    - add fixed rabbit image with transparent background

--- src/js/scenes/openning.js
+++ src/js/scenes/openning.js
@@ -8,17 +8,20 @@ export default class Openning extends Phaser.Scene {
   preload() {
     console.log('in op preload()');
     this.load.spritesheet('rabbit',
-      'assets/player&enemies/rabbit-sprite.png',
+      '../assets/player&enemies/rabbit-sprite.png',
       { frameWidth: 54, frameHeight: 90 });
+    // this.load.image('rock-b', '../assets/bg&objects/Object_2.png');
     for (let i = 0; i < 500; i += 1) {
       this.load.spritesheet(`rabbit + ${i}`,
-        'assets/player&enemies/rabbit-sprite.png',
+        '../assets/player&enemies/rabbit-sprite.png',
         { frameWidth: 54, frameHeight: 90 });
+      // this.load.image(`rock-b + ${i}`, '../assets/bg&objects/Object_2.png');
     }
   }
 
   create() {
     console.log('in op create()');
     this.add.sprite(400, 300, 'rabbit');
+    // this.add.image(400, 300, 'rock-b');
   }
 }
