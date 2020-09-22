4b34e174d468528d8920ad1e8c63805b4168a4fc
NewIncome <alfredo_cardenas@live.com>
2020-09-22 13:49:29 -0500

    Almost totally refactor largeLand making

--- src/helpers/groundLoader.js
+++ src/helpers/groundLoader.js
@@ -11,6 +11,22 @@ const GndCreate = (() => {
     }
   };
 
+  const layerXextRepeater = (x, y, scene, tTile, mTile, bTile, landSize, n) => {
+    for (let i = 0; i < (n * 50); i += 50) {
+      scene.add.image(x + i, y, tTile).setScale(0.2);
+      repeatTileOnX(x + i, y, scene, mTile, landSize);
+      scene.add.image(x + i, y + ((landSize + 1) * 50), bTile).setScale(0.2);
+    }
+  };
+
+  const layerYextRepeater = (x, y, scene, lTile, mTile, rTile, landSize, n) => {
+    for (let i = 0; i < (n * 50); i += 50) {
+      scene.add.image(x, y + i, lTile).setScale(0.2);
+      repeatTileOnX(x, y + i, scene, mTile, landSize);
+      scene.add.image(x + ((landSize + 1) * 50), y + i, rTile).setScale(0.2);
+    }
+  };
+
   // ---------- FLAT Lands ----------
   const makeFlatLand1 = (xPos, yPos, scene) => {
     scene.add.image(xPos, yPos, 'tile-flat-l').setScale(0.2);
@@ -50,105 +66,88 @@ const GndCreate = (() => {
   };
 
   // ---------- LARGE Lands ----------
-  const makeLgLand1 = (xPos, yPos, scene) => {
-    scene.add.image(xPos, yPos, 'tile-lg-tl').setScale(0.2);
-    repeatTileOnX(xPos, yPos, scene, 'tile-lg-tm', 3);
-    scene.add.image(xPos + 200, yPos, 'tile-lg-tr').setScale(0.2);
+  const makeLgLand1 = (xPos, yPos, scene, size = 3) => {
+    layerYextRepeater(xPos, yPos, scene, 'tile-lg-tl', 'tile-lg-tm', 'tile-lg-tr', size, 1);
 
-    scene.add.image(xPos, yPos + 50, 'tile-lg-ml').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 50, scene, 'tile-lg-mm', 3);
-    scene.add.image(xPos + 200, yPos + 50, 'tile-lg-mr').setScale(0.2);
-    // 2nd middle layer
-    scene.add.image(xPos, yPos + 100, 'tile-lg-ml').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 100, scene, 'tile-lg-mm', 3);
-    scene.add.image(xPos + 200, yPos + 100, 'tile-lg-mr').setScale(0.2);
+    // scene.add.image(xPos, yPos, 'tile-lg-tl').setScale(0.2);
+    // repeatTileOnX(xPos, yPos, scene, 'tile-lg-tm', 3);
+    // scene.add.image(xPos + 200, yPos, 'tile-lg-tr').setScale(0.2);
 
-    scene.add.image(xPos, yPos + 150, 'tile-lg-bl').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 150, scene, 'tile-lg-bm', 3);
-    scene.add.image(xPos + 200, yPos + 150, 'tile-lg-br').setScale(0.2);
+    layerYextRepeater(xPos, yPos + 50, scene, 'tile-lg-ml', 'tile-lg-mm', 'tile-lg-mr', size, 2);
+
+    // scene.add.image(xPos, yPos + 50, 'tile-lg-ml').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 50, scene, 'tile-lg-mm', 3);
+    // scene.add.image(xPos + 200, yPos + 50, 'tile-lg-mr').setScale(0.2);
+    // // 2nd middle layer
+    // scene.add.image(xPos, yPos + 100, 'tile-lg-ml').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 100, scene, 'tile-lg-mm', 3);
+    // scene.add.image(xPos + 200, yPos + 100, 'tile-lg-mr').setScale(0.2);
+
+    layerYextRepeater(xPos, yPos + 150, scene, 'tile-lg-bl', 'tile-lg-bm', 'tile-lg-br', size, 1);
+
+    // scene.add.image(xPos, yPos + 150, 'tile-lg-bl').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 150, scene, 'tile-lg-bm', 3);
+    // scene.add.image(xPos + 200, yPos + 150, 'tile-lg-br').setScale(0.2);
   };
 
-  const makeLgLand2 = (xPos, yPos, scene) => {
-    scene.add.image(xPos, yPos, 'tile-lg-tl').setScale(0.2);
-    repeatTileOnX(xPos, yPos, scene, 'tile-lg-tm', 5);
-    scene.add.image(xPos + 300, yPos, 'tile-lg-tr').setScale(0.2);
+  const makeLgLand2 = (xPos, yPos, scene, size = 5) => {
+    layerYextRepeater(xPos, yPos, scene, 'tile-lg-tl', 'tile-lg-tm', 'tile-lg-tr', size, 1);
 
-    scene.add.image(xPos, yPos + 50, 'tile-lg-ml').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 50, scene, 'tile-lg-mm', 5);
-    scene.add.image(xPos + 300, yPos + 50, 'tile-lg-mr').setScale(0.2);
-    // 2nd middle layer
-    scene.add.image(xPos, yPos + 100, 'tile-lg-ml').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 100, scene, 'tile-lg-mm', 5);
-    scene.add.image(xPos + 300, yPos + 100, 'tile-lg-mr').setScale(0.2);
-    // 3rd middle layer
-    scene.add.image(xPos, yPos + 150, 'tile-lg-ml').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 150, scene, 'tile-lg-mm', 5);
-    scene.add.image(xPos + 300, yPos + 150, 'tile-lg-mr').setScale(0.2);
+    // scene.add.image(xPos, yPos, 'tile-lg-tl').setScale(0.2);
+    // repeatTileOnX(xPos, yPos, scene, 'tile-lg-tm', 5);
+    // scene.add.image(xPos + 300, yPos, 'tile-lg-tr').setScale(0.2);
 
-    scene.add.image(xPos, yPos + 200, 'tile-lg-bl').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 200, scene, 'tile-lg-bm', 5);
-    scene.add.image(xPos + 300, yPos + 200, 'tile-lg-br').setScale(0.2);
+    layerYextRepeater(xPos, yPos + 50, scene, 'tile-lg-ml', 'tile-lg-mm', 'tile-lg-mr', size, 3);
+
+    // scene.add.image(xPos, yPos + 50, 'tile-lg-ml').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 50, scene, 'tile-lg-mm', 5);
+    // scene.add.image(xPos + 300, yPos + 50, 'tile-lg-mr').setScale(0.2);
+    // // 2nd middle layer
+    // scene.add.image(xPos, yPos + 100, 'tile-lg-ml').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 100, scene, 'tile-lg-mm', 5);
+    // scene.add.image(xPos + 300, yPos + 100, 'tile-lg-mr').setScale(0.2);
+    // // 3rd middle layer
+    // scene.add.image(xPos, yPos + 150, 'tile-lg-ml').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 150, scene, 'tile-lg-mm', 5);
+    // scene.add.image(xPos + 300, yPos + 150, 'tile-lg-mr').setScale(0.2);
+
+    layerYextRepeater(xPos, yPos + 200, scene, 'tile-lg-bl', 'tile-lg-bm', 'tile-lg-br', size, 1);
+
+    // scene.add.image(xPos, yPos + 200, 'tile-lg-bl').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 200, scene, 'tile-lg-bm', 5);
+    // scene.add.image(xPos + 300, yPos + 200, 'tile-lg-br').setScale(0.2);
   };
 
   const makeLgLand3 = (xPos, yPos, scene, size = 7) => {
-    scene.add.image(xPos, yPos, 'tile-lg-tl').setScale(0.2);
-    repeatTileOnX(xPos, yPos, scene, 'tile-lg-tm', size);
-    // scene.add.image(xPos + 50, yPos, 'tile-lg-tm').setScale(0.2);
-    // scene.add.image(xPos + 100, yPos, 'tile-lg-tm').setScale(0.2);
-    // scene.add.image(xPos + 150, yPos, 'tile-lg-tm').setScale(0.2);
-    // scene.add.image(xPos + 200, yPos, 'tile-lg-tm').setScale(0.2);
-    // scene.add.image(xPos + 250, yPos, 'tile-lg-tm').setScale(0.2);
-    // scene.add.image(xPos + 300, yPos, 'tile-lg-tm').setScale(0.2);
-    scene.add.image(xPos + ((size + 1) * 50), yPos, 'tile-lg-tr').setScale(0.2);
-    scene.add.image(xPos, yPos + 50, 'tile-lg-ml').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 50, scene, 'tile-lg-mm', size);
-    // scene.add.image(xPos + 50, yPos + 50, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 100, yPos + 50, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 150, yPos + 50, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 200, yPos + 50, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 250, yPos + 50, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 300, yPos + 50, 'tile-lg-mm').setScale(0.2);
-    scene.add.image(xPos + ((size + 1) * 50), yPos + 50, 'tile-lg-mr').setScale(0.2);
-    // 2nd middle layer
-    scene.add.image(xPos, yPos + 100, 'tile-lg-ml').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 100, scene, 'tile-lg-mm', size);
-    // scene.add.image(xPos + 50, yPos + 100, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 100, yPos + 100, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 150, yPos + 100, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 200, yPos + 100, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 250, yPos + 100, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 300, yPos + 100, 'tile-lg-mm').setScale(0.2);
-    scene.add.image(xPos + ((size + 1) * 50), yPos + 100, 'tile-lg-mr').setScale(0.2);
-    // 3rd middle layer
-    scene.add.image(xPos, yPos + 150, 'tile-lg-ml').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 150, scene, 'tile-lg-mm', size);
-    // scene.add.image(xPos + 50, yPos + 150, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 100, yPos + 150, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 150, yPos + 150, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 200, yPos + 150, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 250, yPos + 150, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 300, yPos + 150, 'tile-lg-mm').setScale(0.2);
-    scene.add.image(xPos + ((size + 1) * 50), yPos + 150, 'tile-lg-mr').setScale(0.2);
-    // 4th middle layer
-    scene.add.image(xPos, yPos + 200, 'tile-lg-ml').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 200, scene, 'tile-lg-mm', size);
-    // scene.add.image(xPos + 50, yPos + 200, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 100, yPos + 200, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 150, yPos + 200, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 200, yPos + 200, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 250, yPos + 200, 'tile-lg-mm').setScale(0.2);
-    // scene.add.image(xPos + 300, yPos + 200, 'tile-lg-mm').setScale(0.2);
-    scene.add.image(xPos + ((size + 1) * 50), yPos + 200, 'tile-lg-mr').setScale(0.2);
+    layerYextRepeater(xPos, yPos, scene, 'tile-lg-tl', 'tile-lg-tm', 'tile-lg-tr', size, 1);
 
-    scene.add.image(xPos, yPos + 250, 'tile-lg-bl').setScale(0.2);
-    repeatTileOnX(xPos, yPos + 250, scene, 'tile-lg-bm', 7);
-    // scene.add.image(xPos + 50, yPos + 250, 'tile-lg-bm').setScale(0.2);
-    // scene.add.image(xPos + 100, yPos + 250, 'tile-lg-bm').setScale(0.2);
-    // scene.add.image(xPos + 150, yPos + 250, 'tile-lg-bm').setScale(0.2);
-    // scene.add.image(xPos + 200, yPos + 250, 'tile-lg-bm').setScale(0.2);
-    // scene.add.image(xPos + 250, yPos + 250, 'tile-lg-bm').setScale(0.2);
-    // scene.add.image(xPos + 300, yPos + 250, 'tile-lg-bm').setScale(0.2);
-    scene.add.image(xPos + ((size + 1) * 50), yPos + 250, 'tile-lg-br').setScale(0.2);
+    // scene.add.image(xPos, yPos, 'tile-lg-tl').setScale(0.2);
+    // repeatTileOnX(xPos, yPos, scene, 'tile-lg-tm', size);
+    // scene.add.image(xPos + ((size + 1) * 50), yPos, 'tile-lg-tr').setScale(0.2);
+
+    layerYextRepeater(xPos, yPos + 50, scene, 'tile-lg-ml', 'tile-lg-mm', 'tile-lg-mr', size, 4);
+
+    // scene.add.image(xPos, yPos + 50, 'tile-lg-ml').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 50, scene, 'tile-lg-mm', size);
+    // scene.add.image(xPos + ((size + 1) * 50), yPos + 50, 'tile-lg-mr').setScale(0.2);
+    // // 2nd middle layer
+    // scene.add.image(xPos, yPos + 100, 'tile-lg-ml').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 100, scene, 'tile-lg-mm', size);
+    // scene.add.image(xPos + ((size + 1) * 50), yPos + 100, 'tile-lg-mr').setScale(0.2);
+    // // 3rd middle layer
+    // scene.add.image(xPos, yPos + 150, 'tile-lg-ml').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 150, scene, 'tile-lg-mm', size);
+    // scene.add.image(xPos + ((size + 1) * 50), yPos + 150, 'tile-lg-mr').setScale(0.2);
+    // // 4th middle layer
+    // scene.add.image(xPos, yPos + 200, 'tile-lg-ml').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 200, scene, 'tile-lg-mm', size);
+    // scene.add.image(xPos + ((size + 1) * 50), yPos + 200, 'tile-lg-mr').setScale(0.2);
+
+    layerYextRepeater(xPos, yPos + 250, scene, 'tile-lg-bl', 'tile-lg-bm', 'tile-lg-br', size, 1);
+
+    // scene.add.image(xPos, yPos + 250, 'tile-lg-bl').setScale(0.2);
+    // repeatTileOnX(xPos, yPos + 250, scene, 'tile-lg-bm', 7);
+    // scene.add.image(xPos + ((size + 1) * 50), yPos + 250, 'tile-lg-br').setScale(0.2);
   };
 
   return {
