3902475c107e982f7015424b263e989aa69a2172
NewIncome <alfredo_cardenas@live.com>
2020-09-21 23:12:44 -0500

    Make a helper dir and file to separate helper functions

--- src/js/scenes/game.js
+++ src/js/scenes/game.js
@@ -1,12 +1,18 @@
 import Phaser from 'phaser';
 
+import preLoadBar from '../../helpers/loading-bar';
+
 export default class Game extends Phaser.Scene {
   constructor() {
     super('game');
   }
 
   preload() {
-    this.load.image('game-bg', '../assets/bg&objects/Background.png');
+    const asset = this.load.image('game-bg', '../assets/bg&objects/Background.png');
+    console.log('Asset:');
+    console.log(asset);
+    console.log(asset.tag);
+    preLoadBar(this, asset, 'game-bg');
   }
 
   create() {
