04f1d0432ce6fd5de2fcc5d1dae937415cd8aeca
NewIncome <alfredo_cardenas@live.com>
2020-09-23 18:56:10 -0500

    Fixed issue of player faling
    - problem was in the image it self
    - fix frameWidth for perfect view
    
    Running working great!

--- src/js/characters/rabbit.js
+++ src/js/characters/rabbit.js
@@ -4,13 +4,13 @@ export default function rabbitLoad(scene) {
     { frameWidth: 45, frameHeight: 76 });
   scene.load.spritesheet('rabbit-right-run',
     '../assets/player&enemies/rabbit-sprite_r-2(run).png',
-    { frameWidth: 51, frameHeight: 65 });
+    { frameWidth: 50, frameHeight: 76 });
   scene.load.spritesheet('rabbit-right-jump',
     '../assets/player&enemies/rabbit-sprite_r-3(jump).png',
     { frameWidth: 61, frameHeight: 78 });
   scene.load.spritesheet('rabbit-left-run',
     '../assets/player&enemies/rabbit-sprite_l-1 (run).png',
-    { frameWidth: 65, frameHeight: 66 });
+    { frameWidth: 51, frameHeight: 76 });
   scene.load.spritesheet('rabbit-left-jump',
     '../assets/player&enemies/rabbit-sprite_l-2 (jump).png',
     { frameWidth: 42, frameHeight: 77 });
