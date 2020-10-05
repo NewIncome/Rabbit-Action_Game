d0a97d3dc3c520d1f0d9a61f28eeff9c2387faa3
NewIncome <alfredo_cardenas@live.com>
2020-10-05 17:06:52 -0500

    Add hover action on retry and rank buttons
    - and retry button action

--- src/js/scenes/game-over.js
+++ src/js/scenes/game-over.js
@@ -7,15 +7,62 @@ export default class GameOver extends Phaser.Scene {
 
   create() {
     this.add.text(this.game.config.width / 2,
-      this.game.config.height / 2,
-      'GAME OVER');
+      this.game.config.height / 3,
+      'GAME OVER',
+      { fontSize: '26px' });
 
-    this.add.text(this.game.config.width / 2 + 300,
-      this.game.config.height / 2 + 300,
-      'REPLAY');
+    const retryBtn = this.add.text(this.game.config.width / 2 - 300,
+      this.game.config.height / 1.5,
+      'RETRY', {
+        fontSize: '24px',
+        color: '#fff',
+      });
+    const retryBtnHvr = this.add.text(this.game.config.width / 2 - 300,
+      this.game.config.height / 1.5,
+      'RETRY', {
+        fontSize: '24px',
+        fontStyle: 'bold',
+        color: '#ffc',
+      }).setVisible(false);
 
-    this.add.text(this.game.config.width / 2 - 300,
-      this.game.config.height / 2 + 300,
-      'RANK');
+    const rankBtn = this.add.text(this.game.config.width / 2 + 300,
+      this.game.config.height / 1.5,
+      'RANK', {
+        fontSize: '24px',
+        color: '#fff',
+      });
+    const rankBtnHvr = this.add.text(this.game.config.width / 2 + 300,
+      this.game.config.height / 1.5,
+      'RANK', {
+        fontSize: '24px',
+        fontStyle: 'bold',
+        color: '#ffc',
+      }).setVisible(false);
+
+    // ----- Retry bttn actions -----
+    rankBtn.setInteractive();
+
+    rankBtn.on('pointerover', () => {
+      rankBtnHvr.setVisible(true);
+    });
+
+    rankBtn.on('pointerout', () => {
+      rankBtnHvr.setVisible(false);
+    });
+
+    // ----- Retry bttn actions -----
+    retryBtn.setInteractive();
+
+    retryBtn.on('pointerover', () => {
+      retryBtnHvr.setVisible(true);
+    });
+
+    retryBtn.on('pointerout', () => {
+      retryBtnHvr.setVisible(false);
+    });
+
+    retryBtn.on('pointerup', () => {
+      this.scene.start('game');
+    });
   }
 }
