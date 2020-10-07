9411a4d6ae4bcbabcfb9c1ba5f29b04011196d74
NewIncome <alfredo_cardenas@live.com>
2020-10-06 22:25:45 -0500

    Add and fix pushScore and getScore functions
    - html file in test mode for the api.js

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -1,12 +1,13 @@
-const fetch = require("node-fetch");
+// const fetch = require('node-fetch');
 
 
 const gameName = {
   name: 'Rabbit vs The Aliens',
 };
 
+// eslint-disable-next-line no-unused-vars
 async function getScoreAPI() {
-  const scorePost = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
     method: 'post',
     body: JSON.stringify(gameName),
     headers: { 'Content-Type': 'application/json' },
@@ -16,10 +17,56 @@ async function getScoreAPI() {
     .catch(err => console.log(err));
 }
 
-async function pushScores() {
-  
+async function pushScores(userName, scoreVal) {
+  let dat;
+  const jsonScore = JSON.stringify({ user: userName, score: scoreVal });
+  const options = {
+    method: 'POST',
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+    body: jsonScore,
+  };
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/20n0xhB0lcNUfPpfJzUj/scores/',
+    options)
+    .then(response => response.json())
+    .then(data => {
+      dat = data;
+      return dat;
+    })
+    .catch(err => err);
+
+  console.log(dat);
+  return dat;
+}
+
+async function getScores() {
+  let dat;
+
+  const options = {
+    method: 'GET',
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+  };
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/20n0xhB0lcNUfPpfJzUj/scores/',
+    options)
+    .then(response => response.json())
+    .then(data => {
+      dat = data;
+      return dat;
+    })
+    .catch(err => err);
+
+  let sortDat = [];
+  sortDat = Object.entries(dat).sort((a, b) => {
+    if (b[1].score > a[1].score) return 1;
+    if (b[1].score < a[1].score) return -1;
+    return 0;
+  });
+
+  console.log(sortDat);
+  return sortDat;
 }
 
 
+// export { pushScores, getScores };
 
+// For name: Test1, key: 20n0xhB0lcNUfPpfJzUj
 // For this project name, key: TZYfRdsiWsUoxdQNKoTy
