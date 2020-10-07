9b8dd766cf23645b3a31e64ef895b5a3a4542f98
NewIncome <alfredo_cardenas@live.com>
2020-10-07 13:51:13 -0500

    Try to display scoreBoard
    - no luck yet, error with API calls:
      - "Reference Error: regeneratorRunTime is not defined"
        when calling either the get or push scores

--- src/js/scenes/scores.js
+++ src/js/scenes/scores.js
@@ -1,5 +1,7 @@
 import { center, hover } from '../helpers/position-n-buttons';
 
+import { getScores } from '../api/api';
+
 export default class Scores extends Phaser.Scene {
   constructor() {
     super('scores');
@@ -37,5 +39,23 @@ export default class Scores extends Phaser.Scene {
     menu.on('pointerup', () => {
       this.scene.start('gameOver');
     });
+
+    // ----- Get and Display Scores -----
+    this.add.text(width / 2, 60, 'SCORE-BOARD', {
+      fontSize: '40px',
+      fontStyle: 'bold',
+      color: '#8dc',
+    })
+      .setOrigin(0.5, 0.5);
+
+    const DataStyle = 'font: 23px Calibri; color: white; width: 250px; height: 40px; font-weight: 700;';
+    let j = 170;
+
+    const scoreArr = getScores();
+    for (let i = 0; i < 5; i += 1) {
+      this.add.dom(125, j, 'div', `${DataStyle}`, `${i + 1}.-  ${scoreArr[i][1].user}  ${scoreArr[i][1].score}`).setOrigin(0, 0);
+      this.add.dom(525, j, 'div', `${DataStyle}`, `${i + 6}.-  ${scoreArr[i + 5][1].user}  ${scoreArr[i + 5][1].score}`).setOrigin(0, 0);
+      j += 40;
+    }
   }
 }
